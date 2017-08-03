bb4c398bdc3e471e3724657910bd234b2c3f5637
vishwas <batty.wayn3@gmail.com>
2017-08-03 21:22:55 +0530

    add fcm notifiy async action creator

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,6 +1,9 @@
+import axios from 'axios';
+
 export const INCREMENT_USERS = "INCREMENT_USERS";
 export const DECREMENT_USERS = "DECREMENT_USERS";
 export const SET_USERS = "SET_USERS";
+export const NOTIFICATION_SENT = "NOTIFICATION_SENT";
 
 export function incrementUsersAction() {
   return {
@@ -19,4 +22,24 @@ export function setUsers(count) {
     type: SET_USERS,
       count
   };
+}
+
+export function notificationSentStatusAction(status) {
+  return {
+    type: NOTIFICATION_SENT,
+      status
+  };
+}
+
+export function notifyMeAction(fcm) {
+
+  return dispatch => {
+      axios.get('http://localhost:8080/api/notify/'+fcm).then(res=>{
+          dispatch(notificationSentStatusAction("Success"));
+      }).catch((e) => {
+          console.log("Notifying via fcm failed!!", e);
+          dispatch(notificationSentStatusAction("Failed"));
+          return { error: e }
+      });
+  }
 }
