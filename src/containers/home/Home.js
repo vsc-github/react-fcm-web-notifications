8d140372375c2b89e220c558bebd781d674467fa
vishwas <batty.wayn3@gmail.com>
2017-08-03 21:23:41 +0530

    add notifyMe button and onclick action with hard coded fcm token

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -4,7 +4,8 @@ import Users from '../../components/user/Users';
 import {
     incrementUsersAction,
     decrementUsersAction,
-    setUsers
+    setUsers,
+    notifyMeAction
 } from "../../actions/users";
 
 import "./home.css";
@@ -16,6 +17,7 @@ class Home extends Component {
         this.incrementUsers = this.incrementUsers.bind(this);
         this.decrementUsers = this.decrementUsers.bind(this);
         this.setUsers = this.setUsers.bind(this);
+        this.notifyMe = this.notifyMe.bind(this);
     }
 
     incrementUsers() {
@@ -33,6 +35,10 @@ class Home extends Component {
         dispatch(setUsers(394));
     }
 
+    notifyMe(){
+        const { dispatch } = this.props;
+        dispatch(notifyMeAction("cVE7cjmnmSU:APA91bHAnuvYp_0wbHIA5BaA6z6E_DaLLUDstnKfTU7RlKykAFLJWQdyDF2iV2gtZygmPnuYox0dwgLgjQlnqNBfz9IW2JTXip5Ms6ui44Vq9sao_0YPnxaxdPgtZ5dXrjRxhxRo50vB"));
+    }
 
     render() {
         return (
@@ -41,6 +47,7 @@ class Home extends Component {
                 <button onClick={()=>this.incrementUsers()}>INCREMENT</button>
                 <button onClick={()=>this.decrementUsers()}>DECREMENT</button>
                 <button onClick={()=>this.setUsers()}>Turn to 394!</button>
+                <button onClick={()=>this.notifyMe()}>Notify me!</button>
             </div>
         );
     }
