bc7d7ec7083850651e4e556895b67ba8b2dc7408
vishwas <batty.wayn3@gmail.com>
2017-08-10 11:48:47 +0530

    add setNotificationStatus action

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,36 +1,43 @@
 import axios from 'axios';
 
 export const NOTIFICATION_SENT = "NOTIFICATION_SENT";
+export const FCM_TOKEN_GENERATED = "FCM_TOKEN_GENERATED";
 
+export function updateFcmTokenAction(fcmToken) {
+    return {
+        type: FCM_TOKEN_GENERATED,
+        fcmToken
+    };
+}
 export function notificationSentStatusAction(status) {
-  return {
-    type: NOTIFICATION_SENT,
-      status
-  };
+    return {
+        type: NOTIFICATION_SENT,
+        status
+    };
 }
 
 export function notifyMeAction(fcm) {
 
-  return dispatch => {
-      axios.get('http://localhost:8080/api/notify/'+fcm).then(res=>{
-          dispatch(notificationSentStatusAction("Success"));
-      }).catch((e) => {
-          console.log("Notifying via fcm failed!!", e);
-          dispatch(notificationSentStatusAction("Failed"));
-          return { error: e }
-      });
-  }
+    return dispatch => {
+        axios.get('http://localhost:8080/api/notify/' + fcm).then(res => {
+            dispatch(notificationSentStatusAction("Success"));
+        }).catch((e) => {
+            console.log("Notifying via fcm failed!!", e);
+            dispatch(notificationSentStatusAction("Failed"));
+            return {error: e}
+        });
+    }
 }
 
 export function notifyMeWithDelayAction(fcm) {
 
-  return dispatch => {
-      axios.get('http://localhost:8080/api/notify-with-delay/'+fcm).then(res=>{
-          dispatch(notificationSentStatusAction("Success"));
-      }).catch((e) => {
-          console.log("Notifying via fcm failed!!", e);
-          dispatch(notificationSentStatusAction("Failed"));
-          return { error: e }
-      });
-  }
+    return dispatch => {
+        axios.get('http://localhost:8080/api/notify-with-delay/' + fcm).then(res => {
+            dispatch(notificationSentStatusAction("Success"));
+        }).catch((e) => {
+            console.log("Notifying via fcm failed!!", e);
+            dispatch(notificationSentStatusAction("Failed"));
+            return {error: e}
+        });
+    }
 }
